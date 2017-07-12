8db2f3031de7fcc73541dca1219c5a02cfdcc0b6
Louis-Dominique Dubeau <ldd@lddubeau.com>
2017-07-12 13:58:20 -0400

    General cleanup.

--- test/index.spec.js
+++ test/index.spec.js
@@ -1,125 +1,96 @@
 /* global describe, it, beforeEach,process */
-var  should = require('should')
-	, simple = require('simple-mock')
-  , preprocessor = require('../index')['preprocessor:typescript'][1];
+"use strict";
+
+/* eslint-disable import/no-extraneous-dependencies */
+require("should");
+const simple = require("simple-mock");
+const preprocessor = require("../index")["preprocessor:typescript"][1];
 
 const noop = () => false;
 
-//#region Mocks
-/* Mocks */
-var debug = simple.stub();
-  simple.mock(debug, 'debug', noop);
+const debug = simple.stub();
+simple.mock(debug, "debug", noop);
 
-var logger = simple.stub();
-	simple.mock(logger, 'create').returnWith(debug);
+const logger = simple.stub();
+simple.mock(logger, "create").returnWith(debug);
 
-var config = simple.stub();
+const config = simple.stub();
 
-var scope = {};
-//#endregion Mocks
+const scope = {};
 
-describe("factoryTypeScriptPreprocessor", function(){
-	beforeEach(function(){
-		simple.mock(config, 'tsconfigPath'		, './tsconfig.json');
-		simple.mock(config, 'transformPath'		, noop);
-		simple.mock(config, 'ignorePath'		, undefined);
-		simple.mock(config, 'tsconfigOverrides'	, undefined);
-		simple.mock(config, 'compilerOptions'	, undefined)
-	});
+describe("factoryTypeScriptPreprocessor", () => {
+  beforeEach(() => {
+    simple.mock(config, "tsconfigPath", "./tsconfig.json");
+    simple.mock(config, "transformPath", noop);
+    simple.mock(config, "ignorePath", undefined);
+    simple.mock(config, "tsconfigOverrides", undefined);
+    simple.mock(config, "compilerOptions", undefined);
+  });
 
-	describe("factory", function(){
+  describe("factory", () => {
+    beforeEach(() => {
+      process.env.dontCompile = "true";
+    });
 
-		beforeEach(function(){
-			process.env.dontCompile = "true";
-		})
+    it("Should throw an exception if tsconfig.json was not defined", () => {
+      simple.mock(config, "tsconfigPath", undefined);
+      preprocessor.bind(scope, logger, config)
+        .should.throw("tsconfigPath was not defined");
+    });
 
-		it("Should throw an exception if tsconfig.json was not defined", function(){
+    it("Should throw an exception if transformPath is defined and was not a function", () => {
+      simple.mock(config, "transformPath", {});
+      preprocessor.bind(scope, logger, config)
+        .should.throw("transformPath must be an array or a function");
+    });
 
-			simple.mock(config, 'tsconfigPath', undefined);
+    it("Should not throw an exception if transformPath is defined as a function", () => {
+      simple.mock(config, "transformPath", noop);
+      preprocessor.bind(scope, logger, config)
+        .should.not.throw("transformPath must be an array or a function");
+    });
 
-			(function(){
-				preprocessor.call(scope, logger, config);
-			}).should.throw("tsconfigPath was not defined");
-		});
-
-		it("Should throw an exception if transformPath is defined and was not a function", function(){
-
-			simple.mock(config, 'transformPath', {});
-
-			(function(){
-				preprocessor.call(scope, logger, config);
-			}).should.throw("transformPath must be an array or a function");
-		});
-
-		it("Should not throw an exception if transformPath is defined as a function", function(){
-
-			simple.mock(config, 'transformPath', noop);
-
-			(function(){
-				preprocessor.call(scope, logger, config);
-			}).should.not.throw("transformPath must be an array or a function");
-		});
-
-		it("Should not throw an exception if transformPath is defined as a array of function", function(){
-
-			simple.mock(config, 'transformPath', [noop]);
-
-			(function(){
-				preprocessor.call(scope, logger, config);
-			}).should.not.throw("transformPath must be an array or a function");
-		});
+    it("Should not throw an exception if transformPath is defined as a array of function", () => {
+      simple.mock(config, "transformPath", [noop]);
+      preprocessor.bind(scope, logger, config)
+        .should.not.throw("transformPath must be an array or a function");
+    });
 
 
-		it("Should not throw an exception if ignorePath is not defined", function(){
+    it("Should not throw an exception if ignorePath is not defined", () => {
+      simple.mock(config, "ignorePath", undefined);
+      preprocessor.bind(scope, logger, config)
+        .should.not.throw("ignorePath must be a function");
+    });
 
-			simple.mock(config, 'ignorePath', undefined);
+    it("Should throw an exception if ignorePath is defined and is not a function", () => {
+      simple.mock(config, "ignorePath", {});
+      preprocessor.bind(scope, logger, config)
+        .should.throw("ignorePath must be a function");
+    });
 
-			(function(){
-				preprocessor.call(scope, logger, config);
-			}).should.not.throw("ignorePath must be a function");
-		});
+    it("Should not throw an exception if ignorePath is defined as a function", () => {
+      simple.mock(config, "ignorePath", noop);
+      preprocessor.bind(scope, logger, config)
+        .should.not.throw("ignorePath must be a function");
+    });
 
-		it("Should throw an exception if ignorePath is defined and is not a function", function(){
+    it("Should throw an exception if compilerOptions is not defined as object.", () => {
+      simple.mock(config, "compilerOptions", 1);
+      preprocessor.bind(scope, logger, config)
+        .should.throw("compilerOptions if defined, should be an object.");
 
-			simple.mock(config, 'ignorePath', {});
+      simple.mock(config, "compilerOptions", "string");
+      preprocessor.bind(scope, logger, config)
+        .should.throw("compilerOptions if defined, should be an object.");
 
-			(function(){
-				preprocessor.call(scope, logger, config);
-			}).should.throw("ignorePath must be a function");
-		});
+      simple.mock(config, "compilerOptions", new Date());
+      preprocessor.bind(scope, logger, config)
+        .should.throw("compilerOptions if defined, should be an object.");
 
-		it("Should not throw an exception if ignorePath is defined as a function", function(){
-
-			simple.mock(config, 'ignorePath', noop);
-
-			(function(){
-				preprocessor.call(scope, logger, config);
-			}).should.not.throw("ignorePath must be a function");
-		});
-
-		it("Should throw an exception if compilerOptions is not defined as object.", function(){
-
-		simple.mock(config, 'compilerOptions'	, 1/*a number */);
-			(function asNumber(){
-				preprocessor.call(scope, logger, config);
-			}).should.throw("compilerOptions if defined, should be an object.");
-
-		simple.mock(config, 'compilerOptions'	, 'string'/*a string */);
-			(function asString(){
-				preprocessor.call(scope, logger, config);
-			}).should.throw("compilerOptions if defined, should be an object.");
-
-		simple.mock(config, 'compilerOptions'	, new Date/*a date */);
-			(function asDate(){
-				preprocessor.call(scope, logger, config);
-			}).should.throw("compilerOptions if defined, should be an object.");
-
-		simple.mock(config, 'compilerOptions'	, /regex/i/*a regex */);
-			(function asRegex(){
-				preprocessor.call(scope, logger, config);
-			}).should.throw("compilerOptions if defined, should be an object.");
-
-		});
-
-	});
+      simple.mock(config, "compilerOptions", /regex/i);
+      preprocessor.bind(scope, logger, config)
+        .should.throw("compilerOptions if defined, should be an object.");
+    });
+  });
 });
