c9e2853b8e0df8c38d6f5a081990dba2f70d9d5f
Louis-Dominique Dubeau <ldd@lddubeau.com>
2017-07-12 14:16:15 -0400

    Use Chai's expect rather than should.

--- test/index.spec.js
+++ test/index.spec.js
@@ -2,9 +2,10 @@
 "use strict";
 
 /* eslint-disable import/no-extraneous-dependencies */
-require("should");
+const chai = require("chai");
 const preprocessor = require("../index")["preprocessor:typescript"][1];
 
+const expect = chai.expect;
 const noop = () => false;
 
 const logger = {
@@ -27,61 +28,61 @@ describe("factoryTypeScriptPreprocessor", () => {
 
   it("throws if tsconfig.json is undefined", () => {
     config.tsconfigPath = undefined;
-    preprocessor.bind(scope, logger, config)
-      .should.throw("tsconfigPath is undefined");
+    expect(preprocessor.bind(scope, logger, config))
+      .to.throw("tsconfigPath is undefined");
   });
 
   it("throws if transformPath is defined and was not a function", () => {
     config.transformPath = {};
-    preprocessor.bind(scope, logger, config)
-      .should.throw("transformPath must be an array or a function");
+    expect(preprocessor.bind(scope, logger, config))
+      .to.throw("transformPath must be an array or a function");
   });
 
   it("does not throw if transformPath is a function", () => {
     config.transformPath = noop;
-    preprocessor.bind(scope, logger, config)
-      .should.not.throw("transformPath must be an array or a function");
+    expect(preprocessor.bind(scope, logger, config))
+      .to.not.throw("transformPath must be an array or a function");
   });
 
   it("throws if transformPath is an array of function", () => {
     config.transformPath = [noop];
-    preprocessor.bind(scope, logger, config)
-      .should.not.throw("transformPath must be an array or a function");
+    expect(preprocessor.bind(scope, logger, config))
+      .to.not.throw("transformPath must be an array or a function");
   });
 
   it("does not throw if ignorePath is undefined", () => {
     config.ignorePath = undefined;
-    preprocessor.bind(scope, logger, config)
-      .should.not.throw("ignorePath must be a function");
+    expect(preprocessor.bind(scope, logger, config))
+      .to.not.throw("ignorePath must be a function");
   });
 
   it("throws if ignorePath is defined and is not a function", () => {
     config.ignorePath = {};
-    preprocessor.bind(scope, logger, config)
-      .should.throw("ignorePath must be a function");
+    expect(preprocessor.bind(scope, logger, config))
+      .to.throw("ignorePath must be a function");
   });
 
   it("does not throw an exception if ignorePath is a function", () => {
     config.ignorePath = noop;
-    preprocessor.bind(scope, logger, config)
-      .should.not.throw("ignorePath must be a function");
+    expect(preprocessor.bind(scope, logger, config))
+      .to.not.throw("ignorePath must be a function");
   });
 
   it("throws if compilerOptions is not defined as object.", () => {
     config.compilerOptions = 1;
-    preprocessor.bind(scope, logger, config)
-      .should.throw("compilerOptions if defined, should be an object.");
+    expect(preprocessor.bind(scope, logger, config))
+      .to.throw("compilerOptions if defined, should be an object.");
 
     config.compilerOptions = "string";
-    preprocessor.bind(scope, logger, config)
-      .should.throw("compilerOptions if defined, should be an object.");
+    expect(preprocessor.bind(scope, logger, config))
+      .to.throw("compilerOptions if defined, should be an object.");
 
     config.compilerOptions = new Date();
-    preprocessor.bind(scope, logger, config)
-      .should.throw("compilerOptions if defined, should be an object.");
+    expect(preprocessor.bind(scope, logger, config))
+      .to.throw("compilerOptions if defined, should be an object.");
 
-    preprocessor.bind(scope, logger, config)
-      .should.throw("compilerOptions if defined, should be an object.");
     config.compilerOptions = /moo/i;
+    expect(preprocessor.bind(scope, logger, config))
+      .to.throw("compilerOptions if defined, should be an object.");
   });
 });
